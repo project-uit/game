1b541564580df63bf80400e62c6fdf63e62f0d37
Dieu Nguyen <37794414+dieu556@users.noreply.github.com>
2019-04-28 15:51:38 +0700

    tile

---
+++ Tile/MapEditor/GameObject/GameObj.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapEditor.GameObject
+{
+    public enum TypeObj
+    {
+        PhiTieuObj = 001,
+        obj2 = 002,
+    }
+
+    class GameObj
+    {
+        public int Id;
+        public Rectangle bounds;
+        public TypeObj Type;
+        public Image Image;
+
+        public GameObj(int id, Rectangle bounds, TypeObj type, Image image)
+        {
+            this.Id = id;
+            this.bounds = bounds;
+            this.Type = type;
+            this.Image = image;
+        }
+    }
+}
